05118943d4f95b017752863d7cceb86620c3e077
Jiro2784 <gannbare.jiro2@gmail.com>
2025-03-11 15:44:10 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+// config.js
+const CONFIG = {
+  // Apps Script WebアプリのURL
+  APPS_SCRIPT_WEBAPP_URL: 'https://script.google.com/macros/s/AKfycbxOuO0YbbO4qD9_7Dg-Idtu03U7DOlXK8uVRpRSFBwkmD0nmxbDvWAtkefrpOAAUsmf/exec',
+  
+  // 店舗ごとのGoogleマップ口コミURL
+  STORE_REVIEW_URLS: {
+    'QUARTER': 'https://g.page/r/CfiWzYV0WLCdEBE/review',
+    'QUARTER RESORT': 'https://g.page/r/CUpu9_cAhdaGEBE/review',
+    'QUARTER SEASONS': 'https://g.page/r/CWAu_dLl0DJmEBE/review',
+    'LINK': 'https://g.page/r/CYLblbqgWXsREBE/review',
+    'iL': 'https://g.page/r/CemPjkInZSpLEBE/review'
+  }
+};
